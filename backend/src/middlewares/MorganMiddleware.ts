e93c2dbcc1a9fa37f229b8e58bfad9f64030f8eb
Emilie <emilie.ost1990@gmail.com>
2021-11-26 08:29:11 +0100

    added middlewares

--- backend/src/middlewares/MorganMiddleware.ts
+++ backend/src/middlewares/MorganMiddleware.ts
@@ -1,2 +1,21 @@
-import morgan from 'morgan'
+import morgan, {StreamOptions} from 'morgan'
+import Logger from '../utils/Logger'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const stream: StreamOptions = {
+    write: (message: string) => Logger.http(message),
+}
+
+const skip = () => {
+    const env = process.env.ENV_MODE || 'development'
+    return env !== 'development'
+}
+
+const MorganMiddleware = morgan(
+    ':method :url :status :res[content-length] - :response-time ms',
+    {stream, skip}
+)
+
+export default MorganMiddleware
